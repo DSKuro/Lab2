e58306d2146da2b508a98df9af62f4b89e627de2
DSKuro <alex.dutikovsamp@yandex.ru>
2024-10-01 13:53:17 +0300

    base class

---
+++ src/classes/base.py
@@ -0,0 +1,30 @@
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class BaseMaterials:
+    result_cost = 0.0
+    count = 0
+    __metaclass__=ABCMeta
+
+    def __init__(self, width : float, height : float, cost_per_unit : float) -> None:
+        self.width = width
+        self.height = height
+        self.cost_per_unit = cost_per_unit
+
+    def __str__(self) -> str:
+        return f'Значения базового класса: цена: {self.count}, количество: {self.result_cost}'
+
+    @property
+    def get_count(self) -> int:
+        return self.count
+
+    @property
+    def get_cost(self) -> float:
+        return self.result_cost
+
+    @abstractmethod
+    def calculate_cost(self) -> float:
+        pass
+
+    @abstractmethod
+    def calculate_count(self) -> int:
+        pass
