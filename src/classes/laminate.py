97e911c30f0332883fdc1680f615a0ec8d4e9662
DSKuro <alex.dutikovsamp@yandex.ru>
2024-10-01 16:27:26 +0300

    derived classes

---
+++ src/classes/laminate.py
@@ -0,0 +1,19 @@
+import math
+from base import BaseMaterials
+
+class Laminate(BaseMaterials):
+    def __init__(self, width : float, height : float, cost_per_unit : float, color : tuple) -> None:
+        super().__init__(width, height, cost_per_unit)
+
+    def __str__(self) -> str:
+        return f'Значения класса Laminate: цена: {self.count}, количество: {self.result_cost}'
+
+    def calculate_square(self, width: float, height : float) -> None:
+        self.square = width * height
+
+    def calculate_cost(self, s : float):
+        self.calculate_count(s)
+        self.result_cost = self.get_count * self.get_cost
+
+    def calculate_count(self, s : float):
+        self.count = math.ceil(s / self.get_square)
